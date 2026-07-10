e06fc69ce7705864c74f24be64b5b3cfc67f54f7
goyalpratham-7890 <goyalpratham7890@gmail.com>
2026-07-10 15:24:00 +0530

    Special Operators

---
+++ C/Operators/special_operators-1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    a=(printf("Jenny\n",7));
+    printf("%d",a);
+}
+/* Here , separates the 7 and its been declared as extra argument
+there and as output we get count of Jenny+1(\n) as 6 */
---
+++ C/Operators/special_operators-2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    a=(printf("Jenny\n"),7);
+    printf("%d",a);
+}
+/*Here we first separate jenny and 7 by () so 7 will be assigned also*/
