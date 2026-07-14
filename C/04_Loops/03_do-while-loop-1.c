817f69229c66f3796d113ef964fba9d8145c574f
goyalpratham-7890 <goyalpratham7890@gmail.com>
2026-07-14 21:08:34 +0530

    Loops

---
+++ C/04_Loops/03_do-while-loop-1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i=0;
+    do
+    {
+        printf("Jenny");
+    }
+    while(i<0);
+    printf("\nIN BLOCK STATEMENTS");
+    printf("\nOut of loop");
+}
