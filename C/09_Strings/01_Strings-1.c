4d1e53c110173424f06aa88ab0dc059efdf7b996
goyalpratham-7890 <goyalpratham7890@gmail.com>
2026-07-16 21:56:17 +0530

    Strings

---
+++ C/09_Strings/01_Strings-1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+    {
+        char fname[30],lname[30];
+        printf("Enter name:");
+        scanf("%s%s",fname,lname);
+        printf("%s\n%s",fname,lname);
+    }
